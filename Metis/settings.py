b5afd261cd84698472ad0ffed3095072355c5940
EliasEugiii <88091408+EliasEugiii@users.noreply.github.com>
2022-10-21 23:41:00 +0200

    Yeay nur noch ein Fehler
    1234

--- Metis/settings.py
+++ Metis/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@k6k)sicv4#+6=rg#gg7r8igy7f6@6h*0q3%zrpki@dk2#pc30'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
