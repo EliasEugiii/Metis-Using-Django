61d819e06793943290df0cadec9e430206837425
EliasEugiii <88091408+EliasEugiii@users.noreply.github.com>
2022-10-22 00:16:35 +0200

    test 2.0.1
    d

--- Metis/settings.py
+++ Metis/settings.py
@@ -80,6 +80,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Metis.wsgi.application'
 
+CSRF_TRUSTED_ORIGINS = ['*']
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_REPLACE_HTTPS_REFERER = True
+
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
